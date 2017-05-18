dfabedd901363fff1ffd2616cbca95151cb83031
joshmaglione <joshmaglione@gmail.com>
2017-05-18 16:15:48 -0600

    Updated doc format.

--- src/GlobalVars.m
+++ src/GlobalVars.m
@@ -1,7 +1,7 @@
 /*
   Global variables
 */
-__VERSION := "0.1";
+__VERSION := "1.1";
 __SANITY_CHECK := false;
 __LIST := { ModTupFld, ModFld, ModMatFld }; // suitable types we can do most computations with.
 __FRAME := function( T ) // returns the 'domain' and the 'codomain'.
