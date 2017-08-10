a8731b67b93d8c24e3747212e5aac607be381034
joshmaglione <joshmaglione@gmail.com>
2017-08-09 21:55:29 -0400

    Version 1.2.0
    There have been several bug fixes and performance improvements since 1.1.0.

--- src/GlobalVars.m
+++ src/GlobalVars.m
@@ -5,7 +5,7 @@
 
 
 /*  Global variables  */
-__VERSION := "1.1.0";
+__VERSION := "1.2.0";
 __SANITY_CHECK := false;
 __LIST := { ModTupFld, ModFld, ModMatFld }; // suitable types we can do most computations with.
 __FRAME := function( T ) // returns the 'domain' and the 'codomain'.
