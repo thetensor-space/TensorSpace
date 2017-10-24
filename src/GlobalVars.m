712fe0485546bb13d494dd813c2f63ecbc1857c1
joshmaglione <joshmaglione@gmail.com>
2017-10-24 15:27:51 -0400

    Bug fixes
    Updated to version 1.2.3. Fixed a fairly serious bug. It turns out that if M is a free R-module that M.i is *not* necessarily the i-th basis vector.

--- src/GlobalVars.m
+++ src/GlobalVars.m
@@ -5,7 +5,7 @@
 
 
 /*  Global variables  */
-__VERSION := "1.2.2";
+__VERSION := "1.2.3";
 __SANITY_CHECK := false;
 __LIST := { ModTupFld, ModFld, ModMatFld }; // suitable types we can do most computations with.
 __FRAME := function( T ) // returns the 'domain' and the 'codomain'.
